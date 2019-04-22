768052d97fc36102df6cc594fdfeeafff7778681
Trevor <kling109@mail.chapman.edu>
2019-04-22 10:48:59 -0700

    File

---
+++ rms.cpp
@@ -0,0 +1,16 @@
+/*
+Name: Trevor Kling
+ID: 002270716
+Email: kling109@mail.chapman.edu
+Course: CPSC 380 Operating Systems
+Last Date Modified: 22 April 2019
+Project: Rate Monatonic Scheduler
+*/
+
+/*
+Main method.  Will be used to initialize and run threads.
+*/
+int main()
+{
+  return 0;
+}
