19e8b5522f19c603ee752e185ca2923a6aa205eb
Trevor <kling109@mail.chapman.edu>
2019-04-23 12:40:51 -0700

    Added Semaphore

--- rms.cpp
+++ rms.cpp
@@ -7,7 +7,22 @@ Last Date Modified: 22 April 2019
 Project: Rate Monatonic Scheduler
 */
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <pthread.h>
+
+using namespace std;
+
+#define NUM_THREADS 4
+
 int* BOARD[10];
+chrono::milliseconds period(10);
+sem_t wakeupSchedule;
+int t1 = 0;
+int t2 = 0;
+int t3 = 0;
+int t4 = 0;
 
 /*
 Idling work function. Designed to promote cache misses and other small errors.
@@ -29,9 +44,18 @@ void doWork()
 
 /*
 Main method.  Will be used to initialize and run threads.
+Also initializes a semaphore.  The semaphore, with value of 0,
+causes any thread that calls sem_wait to wait until a sem_signal
+is called.  This ensures that, after a thread finishes its work, it waits to
+be rescheduled until the sleep of the original thread finishes.
 */
 int main()
 {
+  if (sem_init(&wakeupSchedule, 0, 0) == -1)
+  {
+    cout << "The semaphore failed to initialize." << endl;
+    return 1;
+  }
   for (int i = 0; i < 10; ++i)
   {
     int* row = new int[10];
@@ -41,5 +65,14 @@ int main()
     }
     BOARD[i] = row;
   }
+  pthread_t threads[NUM_THREADS];
+  int t1Expected = 0;
+  int t2Expected = 0;
+  int t3Expected = 0;
+  int t4Expected = 0;
+
+
+  std::this_thread::sleep_for(16 * period);
+
   return 0;
 }
